0f6d2dcaeacea6bc711f3b3190f965e779158d20
Thanh <thanhhvnqb@gmail.com>
2023-08-10 11:43:41 +0900

    udpate requirement for pycocotools and change train.sh

--- train.sh
+++ train.sh
@@ -1,1 +1,7 @@
-yolo detect train data=coco.yaml model=$1.yaml name=trainval/$1 imgsz=640 batch=64 epochs=100 close_mosaic=10 workers=8 device=0 pretrained=False
+if [ "$1" = "pose" ]; then
+    yolo pose train data=coco-pose.yaml model=$2 name=trainval/$2 imgsz=640 batch=64 epochs=100 close_mosaic=0 workers=8 device=0 pretrained=False
+elif  [ "$1" = "segment" ]; then
+    yolo sement train data=coco.yaml model=$2 name=trainval/$2 imgsz=640 batch=64 epochs=100 close_mosaic=10 workers=8 device=0 pretrained=False
+else    
+    yolo detect train data=coco.yaml model=$2 name=trainval/$2 imgsz=640 batch=64 epochs=100 close_mosaic=10 workers=8 device=0 pretrained=False
+fi
